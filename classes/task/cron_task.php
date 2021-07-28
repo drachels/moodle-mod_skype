733b68062fa8c7d73c10bd4746e5e097b34b5964
AL Rachels <drachels@drachels.com>
2021-07-28 08:42:18 -0500

    Moved cron
    Moved cron from lib.php to tasks.

---
+++ classes/task/cron_task.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_skype\task;
+defined('MOODLE_INTERNAL') || die();
+use context_module;
+use stdClass;
+
+/**
+ * A schedule task for skype cron.
+ *
+ * @package   mod_skype
+ * @copyright 2021 AL Rachels <drachels@drachels.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class cron_task extends \core\task\scheduled_task {
+
+    // Use the logging trait to get some nice, juicy, logging.
+    use \core\task\logging_trait;
+
+    /**
+     * Get a descriptive name for this task (shown to admins).
+     *
+     * @return string
+     */
+    public function get_name() {
+        return get_string('crontask', 'mod_skype');
+    }
+
+    /**
+     * Run skype cron.
+     *
+     * This function searches for things that need to be done, such
+     * as sending out mail, toggling flags etc ...
+     * @return boolean
+     */
+    public function execute() {
+        return true;
+    }
+}
