1f4ef4ca05f1ba5e281e8a3cd71283b7d899b355
AL Rachels <drachels@drachels.com>
2022-03-21 11:56:28 -0500

    Update upgrade.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -31,7 +31,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die(); // @codingStandardsIgnoreLine
 
 /**
  * xmldb_skype_upgrade
