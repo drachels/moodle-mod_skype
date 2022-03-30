4554faf5ef0b4e4d869d670a705666f64a3d397f
AL Rachels <drachels@drachels.com>
2022-03-30 10:12:59 -0500

    Added alerts for debugging

--- js/skypecheck.js
+++ js/skypecheck.js
@@ -29,16 +29,21 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-var activex = ((navigator.userAgent.indexOf('Win') != -1)
-              + && (navigator.userAgent.indexOf('MSIE') != -1)
-              + && (parseInt(navigator.appVersion) >= 4));
-var CantDetect = ((navigator.userAgent.indexOf('Safari') != -1)
-                 + || (navigator.userAgent.indexOf('Opera') != -1));
+window.alert("1 Made it to the skypeCheck.js file.");
+
+var activex = ((navigator.userAgent.indexOf('Win') != -1) &&
+              + (navigator.userAgent.indexOf('MSIE') != -1) &&
+              + (parseInt(navigator.appVersion) >= 4));
+//var CantDetect = '';
+var CantDetect = ((navigator.userAgent.indexOf('Safari') != -1) ||
+                 + (navigator.userAgent.indexOf('Opera') != -1));
 var w,
     h,
     oopswindow,
     detected,
     isSkypeInstalled;
+
+    window.alert("2 Made it to the skypeCheck.js file. CantDetect = " + CantDetect + " activex = " + activex);
 
 /**
  * Adds oopsPopup object.
@@ -46,12 +51,24 @@ var w,
  * @returns false
  */
 function oopsPopup() {
-    if ((navigator.language && navigator.language.indexOf("ja") != -1)
-       + || (navigator.systemLanguage && navigator.systemLanguage.indexOf("ja") != -1)
-       + || (navigator.userLanguage && navigator.userLanguage.indexOf("ja") != -1)) {
-        var URLtoOpen = "skype_not_found/oops.html";
+
+    window.alert("1 Before the if and the navigator.language is = " + navigator.language + " navigator.language.indexOf('ja') is = " + navigator.language.indexOf('ja'));
+    //window.alert("2 Still before the if and the navigator.systemLanguage is = " + navigator.systemLanguage + " navigator.systemLanguage.indexOf('ja') is = " + navigator.systemLanguage.indexOf('ja'));
+
+    if ((navigator.language && navigator.language.indexOf("ja") != -1) ||
+       + (navigator.systemLanguage && navigator.systemLanguage.indexOf("ja") != -1) ||
+       + (navigator.userLanguage && navigator.userLanguage.indexOf("ja") != -1)) {
+        //var URLtoOpen = "skype_not_found/oops.html";
+        var URLtoOpen = "oops.php";
+
+        window.alert("In the if part.");
+
     } else {
-        var URLtoOpen = "skype_not_found/oops.html";
+        //var URLtoOpen = "skype_not_found/oops.html";
+        var URLtoOpen = "oops.php";
+        // Firefox is coming throught this else.
+        window.alert("In the else part and the URLtoOpen = " + URLtoOpen);
+
     }
     var windowName = "ELA";
     var popW = 540, popH = 305;
@@ -60,12 +77,12 @@ function oopsPopup() {
     h = screen.availHeight;
     var leftPos = (w-popW)/2, topPos = (h-popH)/2;
     oopswindow = window.open(URLtoOpen, windowName,'width=' + popW
-                                                + ',height=' + popH
-                                                + ',scrollbars=' + scrollB
-                                                + ',screenx=' + leftPos
-                                                + ',screeny=' + topPos
-                                                + ',top=' + topPos
-                                                + ',left=' + leftPos);
+                                                + ', height=' + popH
+                                                + ', scrollbars=' + scrollB
+                                                + ', screenx=' + leftPos
+                                                + ', screeny=' + topPos
+                                                + ', top=' + topPos
+                                                + ', left=' + leftPos);
     return false;
 }
 
@@ -85,15 +102,23 @@ if (typeof (detected) == "undefined" && activex) {
 /**
  * Adds skypeCheck object.
  *
+ * Executed when one of the four user buttons, Conference, Chat, Voicemail, or Contact is clicked.
  * @returns true If Skype is detected.
  * @returns oopsPopup If Skype is not detected.
  */
 function skypeCheck() {
+
+    window.alert("3 Made it to the function skypeCheck, and CantDetect is = " + CantDetect + " !activex = " + !activex);
+
+
     if (CantDetect) {
         return true;
     } else if (!activex) {
         var skypeMime = navigator.mimeTypes["application/x-skype"];
         detected = true;
+
+        window.alert("4 Made it to the function skypeCheck, and in the elseif due to !activex = " + !activex + " and skypeMime = " + skypeMime);
+
         if (typeof (skypeMime) == "object") {
             return true;
         } else {
@@ -115,6 +140,9 @@ function skypeCheck() {
  *
  */
 function loadDetection() {
+
+    window.alert("5 Made it to the function loadDetection. ");
+
     if (document.getElementById && document.getElementsByTagName) {
         if (window.addEventListener) {
             window.addEventListener('load', addDetection, false);
@@ -130,13 +158,16 @@ function loadDetection() {
  * @returns skypeCheck
  */
 function addDetection() {
+
+    window.alert("6 Made it to the function addDetection. ");
+
     var pageLinks = document.getElementsByTagName("a");
     for (var i=0; i < pageLinks.length; i++) {
         if (pageLinks[i].childNodes[0] && pageLinks[i].childNodes[0].src) {
-            if ((pageLinks[i].childNodes[0].src.indexOf('download.skype.com\/share\/skypebuttons') != -1
-                + || pageLinks[i].childNodes[0].src.indexOf('mystatus.skype.com') != -1)
-                + && (typeof (pageLinks[i].onclick) == "undefined"
-                + || pageLinks[i].onclick === null)) {
+            if ((pageLinks[i].childNodes[0].src.indexOf('download.skype.com\/share\/skypebuttons') != -1 ||
+                + pageLinks[i].childNodes[0].src.indexOf('mystatus.skype.com') != -1) &&
+                + (typeof (pageLinks[i].onclick) == "undefined" ||
+                + pageLinks[i].onclick === null)) {
                 pageLinks[i].onclick = function sChk() {
                     return skypeCheck();
                 }
