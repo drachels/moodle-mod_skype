8bceb0b84453ce7f546427ea147d8413b60ad4d6
AL Rachels <drachels@drachels.com>
2023-04-18 09:39:00 -0500

    Update view.php

--- view.php
+++ view.php
@@ -33,17 +33,17 @@ require_once(__DIR__ .'/../../config.php');
 require_once(__DIR__ .'/lib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // Course_module ID.
-$n  = optional_param('n', 0, PARAM_INT);  // Skype instance ID - it should be named as the first character of the module.
-$groupid  = optional_param('group', 0, PARAM_INT); // All users.
+$n = optional_param('n', 0, PARAM_INT);  // Skype instance ID - it should be named as the first character of the module.
+$groupid = optional_param('group', 0, PARAM_INT); // All users.
 
 if ($id) {
-    $cm         = get_coursemodule_from_id('skype', $id, 0, false, MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $skype  = $DB->get_record('skype', array('id' => $cm->instance), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_id('skype', $id, 0, false, MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $skype = $DB->get_record('skype', array('id' => $cm->instance), '*', MUST_EXIST);
 } else if ($n) {
-    $skype  = $DB->get_record('skype', array('id' => $n), '*', MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $skype->course), '*', MUST_EXIST);
-    $cm         = get_coursemodule_from_instance('skype', $skype->id, $course->id, false, MUST_EXIST);
+    $skype = $DB->get_record('skype', array('id' => $n), '*', MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $skype->course), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_instance('skype', $skype->id, $course->id, false, MUST_EXIST);
 } else {
     error('You must specify a course_module ID or an instance ID');
 }
